f5dd4239377ed17677bc9a1aacda5b84fbc26bb7
Daniel Gentry <109824286+DanimalCracker5@users.noreply.github.com>
2026-01-12 16:51:48 -0500

    Created CameraControler

---
+++ Cryptivo Bridge Test/Assets/- Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+//Script is using legacy input system mainly due to time limitations
+public class CameraController : MonoBehaviour
+{
+    #region Variables
+    Camera _camera;
+    float pitch, yaw;
+
+    [Range(0.1f, 5f)]
+    public float RotationSensitivity = 2f;
+
+    [Range(5f, 10f)]
+    public float MovementSensitivity = 6f;
+    #endregion
+
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+        UpdateMovement();
+    }
+    private void UpdateMovement()
+    {
+        //Get Inputs
+        float _horizontal = (Input.GetAxis("Horizontal") * MovementSensitivity);
+        float _vertical = (Input.GetAxis("Vertical") * MovementSensitivity);
+
+        //Translate By Inputs
+        transform.Translate(
+            new Vector3(
+                _horizontal * Time.deltaTime,
+                0f,
+                _vertical * Time.deltaTime)
+            );
+    }
+    private void UpdateRotation()
+    {
+        yaw += Input.GetAxis("Mouse X") * RotationSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * RotationSensitivity;
+        _camera.transform.localEulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+}
